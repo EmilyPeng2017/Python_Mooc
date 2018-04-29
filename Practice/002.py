77381cc87774d8be3457436ca8d24fda96a73c65
Emily Peng <emily2016@sjtu.edu.cn>
2018-04-29 17:10:34 +0800

    Python practice_002

--- Practice/002.py
+++ Practice/002.py
@@ -11,11 +11,13 @@
 2.程序源代码：
 '''
 bonus1 = 100000 * 0.1
-bonus2 = bonus1 + 100000 * 0.500075
+bonus2 = bonus1 + 100000 * 0.75
 bonus4 = bonus2 + 200000 * 0.5
 bonus6 = bonus4 + 200000 * 0.3
 bonus10 = bonus6 + 400000 * 0.15
 
+# i = raw_input('input gain: ')
+# TypeError: unsupported operand type(s) for -: 'str' and 'int'
 i = int(raw_input('input gain:\n'))
 if i <= 100000:
     bonus = i * 0.1
@@ -29,4 +31,5 @@ elif i <= 1000000:
     bonus = bonus6 + (i - 600000) * 0.015
 else:
     bonus = bonus10 + (i - 1000000) * 0.01
-print 'bonus = ',bonus
+print 'bonus = ', bonus
+
